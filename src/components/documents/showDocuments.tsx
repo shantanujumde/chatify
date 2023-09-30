63d9bc710768a46b4186d4c73d7a9142d94d9c8b
shantanujumde <jumde21@gmail.com>
2023-09-30 21:04:31 +0530

    pagination in show documents

--- src/components/documents/showDocuments.tsx
+++ src/components/documents/showDocuments.tsx
@@ -1,5 +1,11 @@
 import { api } from "@/utils/api";
-import { CheckIcon, Pencil2Icon, TrashIcon } from "@radix-ui/react-icons";
+import {
+  ArrowLeft,
+  ArrowRight,
+  FileSignature,
+  Save,
+  Trash2,
+} from "lucide-react";
 import { useRef, useState, type FC } from "react";
 import {
   Accordion,
@@ -13,8 +19,9 @@ import Spinner from "../ui/spinner";
 const ShowDocuments: FC = () => {
   const [edit, setEdit] = useState<number | null>();
   const editRef = useRef<HTMLInputElement>(null);
+  const [page, setPage] = useState(1);
 
-  const getDocuments = api.documents.getMyDocuments.useQuery();
+  const getDocuments = api.documents.getMyDocuments.useQuery({ page });
   const renameDocument = api.documents.renameDocumentById.useMutation({
     onSuccess: async () => {
       await getDocuments.refetch();
@@ -41,44 +48,64 @@ const ShowDocuments: FC = () => {
   if (getDocuments.isLoading) return <Spinner />;
 
   if (!getDocuments.data) return <>Empty</>;
-  console.log("documents", getDocuments);
 
   return (
-    <Accordion type="single" collapsible className="w-full">
-      {getDocuments.data.map((document, indx) => {
-        return !document.deleted ? (
-          <AccordionItem key={document.id} value={document.id.toString()}>
-            <div className="m-auto flex">
-              <div className="w-1/2">
-                <AccordionTrigger className="flex justify-between">
-                  <p>{indx + 1}.</p>
+    <>
+      <Accordion type="single" collapsible className="w-full">
+        {getDocuments.data.map((document, indx) => {
+          return !document.deleted ? (
+            <AccordionItem key={document.id} value={document.id.toString()}>
+              <div className="m-auto flex">
+                <div className="w-1/2">
+                  <AccordionTrigger className="flex justify-between">
+                    <p>{indx + 1}.</p>
+                    {edit === document.id ? (
+                      <Input
+                        defaultValue={document.name}
+                        ref={editRef}
+                        className="w-fit"
+                      />
+                    ) : (
+                      <p>{document.name}</p>
+                    )}
+                  </AccordionTrigger>
+                </div>
+                <div className="m-auto flex w-1/2 justify-around">
                   {edit === document.id ? (
-                    <Input defaultValue={document.name} ref={editRef} />
+                    <Save
+                      className="cursor-pointer text-green-400"
+                      onClick={() =>
+                        void handleRename(document.id, editRef.current?.value)
+                      }
+                    />
                   ) : (
-                    <p>{document.name}</p>
+                    <FileSignature
+                      className="cursor-pointer text-blue-400"
+                      onClick={() => setEdit(document.id)}
+                    />
                   )}
-                </AccordionTrigger>
-              </div>
-              <div className="m-auto flex w-1/2 justify-end">
-                {edit === document.id ? (
-                  <CheckIcon
-                    onClick={() =>
-                      void handleRename(document.id, editRef.current?.value)
-                    }
+
+                  <Trash2
+                    className="cursor-pointer text-red-400"
+                    onClick={() => void handleDeleteDocument([document.id])}
                   />
-                ) : (
-                  <Pencil2Icon onClick={() => setEdit(document.id)} />
-                )}
-                <TrashIcon
-                  onClick={() => void handleDeleteDocument([document.id])}
-                />
+                </div>
               </div>
-            </div>
-            <AccordionContent>{document.content}</AccordionContent>
-          </AccordionItem>
-        ) : null;
-      })}
-    </Accordion>
+              <AccordionContent>{document.content}</AccordionContent>
+            </AccordionItem>
+          ) : null;
+        })}
+      </Accordion>
+      <div className="mt-4 flex justify-around">
+        <ArrowLeft
+          onClick={() =>
+            setPage((currPage) => (currPage > 0 ? currPage - 1 : 0))
+          }
+        />
+        Page {page}
+        <ArrowRight onClick={() => setPage((currPage) => currPage + 1)} />
+      </div>
+    </>
   );
 };
 
