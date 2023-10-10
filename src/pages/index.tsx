fe7d9423426e019fe25483550f86f2573b110076
shantanujumde <jumde21@gmail.com>
2023-10-10 21:56:39 +0530

    add gif image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,14 +62,14 @@ export default function Home() {
           <RevealOnScroll>
             <div className="mx-auto max-w-6xl px-6 lg:px-8">
               <div className="mt-16 flow-root sm:mt-24">
-                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <div className="rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <Image
-                    src="/dashboard-preview.jpg"
+                    src="/chat-preview.webp"
                     alt="product preview"
-                    width={1364}
-                    height={866}
                     quality={100}
-                    className="rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-8 md:p-20"
+                    width={1000}
+                    height={498}
+                    className="rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10 sm:p-2"
                   />
                 </div>
               </div>
