0b81d39f94d5fe9b0e4d01c0d4e3a092b32bf134
shantanujumde <jumde21@gmail.com>
2023-11-01 22:38:55 +0530

    invite email to user

--- src/app/inviteUsers/page.tsx
+++ src/app/inviteUsers/page.tsx
@@ -1,7 +1,41 @@
-import { type FC } from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import { api } from "@/utils/api";
+import { useRef, type FC } from "react";
 
 const Page: FC = ({}) => {
-  return <>This is Page</>;
+  const email = useRef<HTMLInputElement | null>(null);
+  const { mutate: sendEmail, isLoading: sendEmailIsLoading } =
+    api.profile.sendEmail.useMutation({
+      onSuccess() {
+        if (email.current?.value) email.current.value = "";
+      },
+      onError(err) {
+        console.log("err", err);
+
+        toast({
+          variant: "destructive",
+          description: "Something went wrong!, please try again",
+        });
+      },
+    });
+
+  const handleEmailSend = () => {
+    if (email.current?.value) sendEmail({ email: email.current.value });
+  };
+
+  return (
+    <div className="flex w-full justify-center">
+      <div className=" flex w-1/2 gap-2">
+        <Input ref={email} />
+        <Button loading={sendEmailIsLoading} onClick={() => handleEmailSend()}>
+          Invite
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,4 +1,7 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { prisma } from "@/server/db";
+import { hash } from "bcrypt";
+import { randomUUID } from "crypto";
 import nodemailer from "nodemailer";
 import { env } from "process";
 import { z } from "zod";
@@ -39,28 +42,56 @@ export const profileRouter = createTRPCRouter({
 
   sendEmail: protectedProcedure
     .input(z.object({ email: z.string() }))
-    .mutation(({ input }) => {
-      const mailer = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-          user: env.EMAIL_USER,
-          pass: env.EMAIL_PASS,
-        },
-      });
-
-      const mailDetails = {
-        from: "noreply@chatify.com",
-        to: input.email,
-        subject: "Test mail",
-        text: "Node.js testing mail for GeeksforGeeks",
-      };
-
-      mailer.sendMail(mailDetails, function (err, data) {
-        if (err) {
-          console.log("Error Occurs");
-        } else {
-          console.log("Email sent successfully", data);
-        }
-      });
+    .mutation(async ({ input }) => {
+      const { email, password } = await createUser(input.email);
+      sendEmail(
+        "Welcome to Chatify",
+        "here is your username & password " + email + " " + password,
+        input.email
+      );
     }),
 });
+
+export const createUser = async (email: string) => {
+  const password = randomUUID();
+  const hashedPassword = await hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { email, password };
+};
+
+export const sendEmail = (
+  subject: string,
+  text: string,
+  emailTo: string,
+  emailFrom = "noreply@chatify.com"
+) => {
+  const mailer = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: env.EMAIL_USER,
+      pass: env.EMAIL_PASS,
+    },
+  });
+
+  const mailDetails = {
+    from: emailFrom,
+    to: emailTo,
+    subject,
+    text,
+  };
+
+  mailer.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log("Error Occurs");
+    } else {
+      console.log("Email sent successfully", data);
+    }
+  });
+};
