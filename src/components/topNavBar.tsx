6f4ca95c3bb767c8c50e073a27a00b8d8958e3ff
shantanujumde <jumde21@gmail.com>
2023-12-23 21:04:37 +0530

    improve nav-bar

--- src/components/topNavBar.tsx
+++ src/components/topNavBar.tsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/navigation-menu";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { cn } from "@/utils/utils";
-import { ChatBubbleIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { ModeToggle } from "./mode";
@@ -88,10 +88,22 @@ export function TopNavBar() {
   return (
     <NavigationMenu
       dropdownLocation="left"
-      className={`fixed inset-x-0 top-5 mx-auto w-11/12 rounded-lg border border-secondary bg-secondary/60 p-2 transition-all duration-500 ease-in-out  ${
+      className={`fixed inset-x-0 top-5 mx-auto w-11/12 rounded-lg border border-secondary bg-secondary p-2  transition-all duration-500 ease-in-out dark:bg-black  ${
         !show ? "opacity-0" : "opacity-100"
       }`}
     >
+      <div
+        aria-hidden="true"
+        className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] h-[25rem] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+        />
+      </div>
       <div className="flex justify-end">
         <Button
           variant="ghost"
@@ -104,13 +116,13 @@ export function TopNavBar() {
       {(showMenu || !isMobile) && (
         <NavigationMenuList className="flex w-full justify-between max-md:flex-col max-md:items-end">
           <div className="flex  gap-1 max-md:flex-col max-md:items-end">
-            <NavigationMenuItem className="rounded-xl border border-gray-400/50">
+            {/* <NavigationMenuItem className="p-0">
               <Link href="/" legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  <ChatBubbleIcon />
+                  <Icons.logo className="h-8 w-8 dark:fill-white" />
                 </NavigationMenuLink>
               </Link>
-            </NavigationMenuItem>
+            </NavigationMenuItem> */}
 
             <NavigationMenuItem className="rounded-xl border border-gray-400/50">
               <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
@@ -122,13 +134,13 @@ export function TopNavBar() {
                         className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                         href="/"
                       >
-                        <Icons.logo className="h-6 w-6" />
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
+                        <Icons.logo className="z-10 dark:fill-white" />
+                        <div className="mb-2 mt-4 text-center text-lg font-medium">
+                          Chatify
                         </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components built with Radix UI
-                          and Tailwind CSS.
+                        <p className="text-center text-sm leading-tight text-muted-foreground">
+                          Worlds best tool to create knowledge-base and
+                          understand the data properly.
                         </p>
                       </Link>
                     </NavigationMenuLink>
