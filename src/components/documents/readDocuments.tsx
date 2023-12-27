30449cd8213347e3ae79c9fabae4f273bc7a7335
shantanujumde <jumde21@gmail.com>
2023-12-27 13:15:50 +0530

    text area to read docs

--- src/components/documents/readDocuments.tsx
+++ src/components/documents/readDocuments.tsx
@@ -2,15 +2,23 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useReadText } from "@/hooks/readTextFromDocument";
 import { api } from "@/utils/api";
-import { File } from "lucide-react";
-import React, { type FC } from "react";
+import React, { useEffect, useState, type FC } from "react";
 import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
 import { toast } from "../ui/use-toast";
 
 const ReadDocuments: FC<{ refetchDocuments: () => Promise<void> }> = ({
   refetchDocuments,
 }) => {
   const [textObj, getTextFromDoc] = useReadText();
+  const [textContent, setTextContent] = useState("");
+
+  useEffect(() => {
+    if (textObj.text) {
+      setTextContent(textObj.text);
+    }
+  }, [textObj]);
+
   const {
     text,
     name,
@@ -48,22 +56,30 @@ const ReadDocuments: FC<{ refetchDocuments: () => Promise<void> }> = ({
     getTextFromDoc(e);
     e.target.value = "";
   };
-
+  const textArea = (
+    <Textarea
+      className="scrollbar-thumb-black scrollbar-track-black-lighter dark:scrollbar-thumb-white  
+dark:scrollbar-track-white-lighter scrollbar-thumb-rounded 
+scrollbar-w-2 scrolling-touch h-60 max-h-max resize-none overflow-auto"
+      value={textContent}
+      onChange={(e) => setTextContent(e.target.value)}
+      placeholder="Type something here..."
+      disabled={loadingDocument}
+    />
+  );
   return (
     <div className="relative flex h-full w-full flex-row justify-between gap-4 max-md:flex-col max-md:items-center">
-      <div className="flex w-1/2 max-w-sm flex-col gap-6 max-md:w-full ">
-        <h2 className="flex ">
-          <span className="text-xl uppercase">
-            <Label htmlFor="pdf">Select pdf</Label>
-          </span>
-        </h2>
+      <div className="ml-4 flex w-1/2 max-w-sm flex-col items-center justify-center gap-6 max-md:w-full">
+        <Label htmlFor="pdf" className="text-xl uppercase underline">
+          Select pdf
+        </Label>
         <Input id="pdf" type="file" accept=".pdf" onChange={handleFileChange} />
 
         <Button
           className="w-full"
           loading={loadingDocument || isCreateEmbeddingLoading}
-          disabled={text.length === 0 || loadingDocument}
-          onClick={() => handleCreateEmbedding(text, name)}
+          disabled={textContent.length === 0 || loadingDocument}
+          onClick={() => handleCreateEmbedding(textContent, name)}
         >
           Save
         </Button>
@@ -73,17 +89,16 @@ const ReadDocuments: FC<{ refetchDocuments: () => Promise<void> }> = ({
         className="scrollbar-thumb-black scrollbar-track-black-lighter  
           dark:scrollbar-thumb-white dark:scrollbar-track-white-lighter 
           scrollbar-thumb-rounded   scrollbar-w-2
-         scrolling-touch flex h-36 max-h-screen w-1/2 flex-col gap-6 overflow-auto overflow-y-scroll rounded-xl bg-gray-300/20 p-6 py-2 pl-4 dark:bg-gray-300/5 max-md:w-full"
+         scrolling-touch flex h-80 max-h-screen w-1/2 flex-col gap-6 overflow-auto overflow-y-scroll rounded-xl bg-gray-300/20 p-6 py-2 pl-4 dark:bg-gray-300/5 max-md:w-full"
       >
         <h2 className="flex justify-center">
           <span className="text-xl uppercase underline">Content</span>
         </h2>
         {text ? (
-          <p>{text}</p>
+          <>{textArea}</>
         ) : (
-          <div className="flex w-full flex-col items-center gap-2 ">
-            <File className="h-8 w-8" />
-            <p className="">Select file to preview content</p>
+          <div className="flex w-full flex-col items-center gap-2 align-middle">
+            {textArea}
           </div>
         )}
       </div>
