61d5b5025fd043a37479b59194804b1441d740bf
shantanujumde <jumde21@gmail.com>
2023-12-27 21:29:59 +0530

    update style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+      <div className="mx-auto mb-4 flex max-w-fit cursor-pointer items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
         <p className="text-sm font-semibold text-gray-700">
           Chatify is proudly open-source on Github ❤️
         </p>
