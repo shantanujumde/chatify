aeefe6cb0600f12ccda110b204290299bea41d5a
shantanujumde <jumde21@gmail.com>
2024-02-04 16:53:48 +0530

    fix date error

--- src/server/api/routers/freeTrial.ts
+++ src/server/api/routers/freeTrial.ts
@@ -3,21 +3,23 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 export const freeTrialRouter = createTRPCRouter({
   createFreeTrail: protectedProcedure.mutation(async ({ ctx }) => {
     const user = ctx.session.user;
+    const dateAfterOneYear = new Date(
+      new Date().setFullYear(new Date().getFullYear() + 1)
+    );
+
     await ctx.prisma.payment.upsert({
       create: {
         stripeCustomerId: "freeTrial",
         stripeSubscriptionId: "freeTrial",
         stripePriceId: "freeTrial",
-        stripeCurrentPeriodEnd: "freeTrial",
+        stripeCurrentPeriodEnd: dateAfterOneYear,
         userId: user.id,
       },
       update: {
         stripeCustomerId: "freeTrial",
         stripeSubscriptionId: "freeTrial",
         stripePriceId: "freeTrial",
-        stripeCurrentPeriodEnd: new Date(
-          new Date().setFullYear(new Date().getFullYear() + 1)
-        ),
+        stripeCurrentPeriodEnd: dateAfterOneYear,
         userId: user.id,
       },
       where: {
