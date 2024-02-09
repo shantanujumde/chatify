daf8b94967262fb14510e0c52b23bc8de2edc663
shantanujumde <jumde21@gmail.com>
2024-02-09 10:12:02 +0530

    free trial

--- src/components/freeTrial.tsx
+++ src/components/freeTrial.tsx
@@ -1,11 +1,10 @@
-import { ArrowRight, Check, HelpCircle, MessageCircle } from "lucide-react";
+import { ArrowRight, Check, HelpCircle, Minus } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { type FC } from "react";
 import { api } from "../utils/api";
 import { cn } from "../utils/utils";
 import { Button, buttonVariants } from "./ui/button";
-import { Input } from "./ui/input";
 import {
   Tooltip,
   TooltipContent,
@@ -48,39 +47,76 @@ const FreeTrial: FC = () => {
             </h3>
             <p className="text-gray-500 dark:text-gray-300">Try for free!</p>
             <p className="font-display my-5 text-6xl font-semibold">$0</p>
-            <p className="text-gray-500 dark:text-gray-300">per month</p>
+            <p className="text-gray-500 dark:text-gray-300">
+              per month (no credit card required!)
+            </p>
           </div>
           <ul className="my-10 space-y-5 px-8">
-            <li className="flex space-x-5">
-              <div className="flex-shrink-0">
-                <Check className="h-6 w-6 text-primary/80" />
-              </div>
+            {[
+              {
+                text: "Create a knowledge base of 2 files.",
+                footnote:
+                  "You can store up to 2 files. Each file can have more than 3000+ characters.",
+              },
+              {
+                text: "Up to 100 chat messages.",
+                footnote:
+                  "You can have up to 100 chat messages. And understand every aspects of your file",
+              },
+              {
+                text: "Mobile-friendly interface for on-the-go access.",
+              },
+              {
+                text: "Priority support to address your business needs promptly.",
+              },
+              {
+                text: "Limited collaborators",
+                footnote:
+                  "Users that can access knowledge base (You can assign them roles, individuals that controls knowledge base & individuals that can only chat, etc.).",
+              },
+              {
+                text: "Role-based access control (Coming soon).",
+                footnote:
+                  "Users that can access knowledge base (You can assign them roles, individuals that controls knowledge base & individuals that can only chat, etc.).",
+              },
+              {
+                text: "Very fast & higher quality responses.",
+                footnote:
+                  "Expect slower responses as compared to Business plan having better algorithmic responses for enhanced content quality.",
 
-              <div className="flex items-center space-x-1">
-                <p className={cn("text-gray-600 dark:text-gray-300")}>
-                  Use your own OpenAI API key & Access all Chatify&apos;s
-                  Business plan features for free.
-                </p>
-                <Tooltip delayDuration={300}>
-                  <TooltipTrigger className="ml-1.5 cursor-default">
-                    <HelpCircle className="h-4 w-4 text-zinc-500" />
-                  </TooltipTrigger>
-                  <TooltipContent className="w-80 p-2">
-                    Leverage your personal OpenAI API key to seamlessly unlock
-                    all premium features of Chatify&apos;s Business plan,
-                  </TooltipContent>
-                </Tooltip>
-              </div>
-            </li>
-            <li className="flex space-x-5">
-              <div className="flex-shrink-0">
-                <MessageCircle className="top-3 h-6 w-6 text-primary/80" />
-              </div>
-              <Input
-                className="w-full"
-                placeholder="Enter your OpenAI API key"
-              />
-            </li>
+                negative: true,
+              },
+              {
+                text: "Unlimited collaborators",
+                negative: true,
+              },
+            ].map((feature) => (
+              <li key={feature.text} className="flex space-x-5">
+                <div className="flex-shrink-0">
+                  {!feature.negative ? (
+                    <Check className="h-6 w-6 text-primary/80" />
+                  ) : (
+                    <Minus className="h-6 w-6 text-gray-300 dark:text-gray-500" />
+                  )}
+                </div>
+
+                <div className="flex items-center space-x-1">
+                  <p className={cn("text-gray-600 dark:text-gray-300")}>
+                    {feature.text}
+                  </p>
+                  {feature.footnote && (
+                    <Tooltip delayDuration={300}>
+                      <TooltipTrigger className="ml-1.5 cursor-default">
+                        <HelpCircle className="h-4 w-4 text-zinc-500" />
+                      </TooltipTrigger>
+                      <TooltipContent className="w-80 p-2">
+                        {feature.footnote}
+                      </TooltipContent>
+                    </Tooltip>
+                  )}
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="h-1/6 border-t border-gray-200">
