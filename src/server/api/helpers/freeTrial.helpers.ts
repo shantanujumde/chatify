253b58dd80bb0a4f4be08d972f8e11e12aa4a090
shantanujumde <jumde21@gmail.com>
2024-02-15 09:55:42 +0530

    little fix

--- src/server/api/helpers/freeTrial.helpers.ts
+++ src/server/api/helpers/freeTrial.helpers.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../db";
 import { type Context } from "../trpc";
+import { getOrganizationId } from "./profile.helpers";
 
 export const CHAT_LIMIT = 100;
 export const FILES_LIMIT = 2;
@@ -14,9 +15,11 @@ export const chatLimit = async (userId: string) => {
 };
 
 export const fileLimit = async (ctx: Context) => {
+  const organizationId = await getOrganizationId(ctx);
+
   const fileCount = await ctx.prisma.organization.findFirst({
     where: {
-      id: ctx.session?.user.organizationId,
+      id: organizationId,
     },
     include: {
       _count: {
