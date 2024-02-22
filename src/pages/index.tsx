4f14a38ff4bab48d4a9cbcfd29056d772b099553
shantanujumde <jumde21@gmail.com>
2024-02-22 19:44:12 +0530

    change brand icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { ArrowRight } from "lucide-react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { Icons } from "../components/ui/icons";
 import { BRAND_NAME } from "../utils/utils";
 
 // t3- https://www.youtube.com/watch?v=J1gzN1SAhyM
@@ -24,7 +25,8 @@ export default function Home() {
       <InvitationDialog />
       <div className="mx-auto mb-4 flex max-w-fit cursor-pointer items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
         <p className="text-sm font-semibold text-gray-700">
-          {BRAND_NAME} is proudly open-source on Github ❤️
+          <Icons.logoShort className="inline h-5 w-5" /> is proudly open-source
+          on Github ❤️
         </p>
       </div>
       <div className="-mt-24 flex h-screen flex-col items-center justify-center">
