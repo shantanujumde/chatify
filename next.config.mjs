db87ee0736979a39baf4b7f8124a53d22dd0c77a
shantanujumde <jumde21@gmail.com>
2024-03-08 13:07:15 +0530

    fix eslint

--- next.config.mjs
+++ next.config.mjs
@@ -21,9 +21,6 @@ const config = {
   //   locales: ["en"],
   //   defaultLocale: "en",
   // },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 
   webpack: function webpack(config) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
